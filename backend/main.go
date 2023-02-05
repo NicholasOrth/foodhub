53fe9ffcf16deae6a0f176e61c55deb814888309
slinky55 <larry.mason@ufl.edu>
2023-02-05 16:11:31 -0500

    updated main.go, added 2 API routes

--- backend/main.go
+++ backend/main.go
@@ -1,23 +1,58 @@
 package main
 
 import (
-  "github.com/gorilla/mux"
-  "net/http"
-  "os"
-  "log"
-  "fmt"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
 )
 
+type User struct {
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func createUser(user User) {
+	log.Println(user)
+}
+
 func main() {
-  fmt.Println("Starting server")
-  r := mux.NewRouter()
+	log.Println("Starting server...")
 
-  http.Handle("/", r)
+	router := gin.Default()
 
-  srv := &http.Server {
-    Handler: r,
-    Addr:    ":" + os.Getenv("PORT"),
-  }
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type"},
+		AllowCredentials: true,
+		AllowWildcard:    true,
+	}))
 
-  log.Fatal(srv.ListenAndServe())
+	router.GET("/ping", func(c *gin.Context) {
+		c.IndentedJSON(http.StatusOK, gin.H{"message": "pong"})
+	})
+
+	router.GET("/user/:id", func(c *gin.Context) {
+
+	})
+
+	router.POST("/user", func(c *gin.Context) {
+		var user User
+		if err := c.ShouldBindJSON(&user); err != nil {
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		createUser(user)
+		c.Header("Content-Type", "application/json")
+		c.JSON(http.StatusOK, user)
+	})
+
+	err := router.Run(":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
