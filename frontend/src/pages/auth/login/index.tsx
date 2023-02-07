823320acc3ea77fa22fe4695321e4d6a94941324
slinky55 <larry.mason@ufl.edu>
2023-02-07 11:51:32 -0500

    fixed auth pages

--- frontend/src/pages/auth/login/index.tsx
+++ frontend/src/pages/auth/login/index.tsx
@@ -1,7 +1,8 @@
 import React, {useState} from "react"
 
+import Router from "next/router"
 
-export const Index = (props: any) => {
+export default function Login() {
     const[email, setEmail] = useState('');
     const[password,setPassword] = useState('');
 
@@ -11,15 +12,21 @@ export const Index = (props: any) => {
     }
     return(
         <div className="authform">
-     <form onSubmit={handleSubmit}> 
-        <label htmlFor="email">email</label>
-        <input value={email} onChange={(e) => setEmail(e.target.value)} type ="email" placeholder="email@whatevermail.com" id="email" name="email"/>
-        <label htmlFor="password">email</label>
-        <input value={password} onChange={(e) => setPassword(e.target.value)} type="passsword" placeholder="**********" id="password" name="name"/>
-        <button> Log In</button>
-     </form>
-     <button onClick={() => props.onFormSwitch('register')}>Dont Have an account? Register here. </button>
-     </div>
+         <form onSubmit={handleSubmit}>
+            <label htmlFor="email">email</label>
+            <input value={email} onChange={(e) => setEmail(e.target.value)} type ="text" placeholder="email@whatevermail.com" id="email" name="email"/>
+            <label htmlFor="password">email</label>
+            <input value={password} onChange={(e) => setPassword(e.target.value)} type="passsword" placeholder="**********" id="password" name="name"/>
+            <button> Log In</button>
+         </form>
+
+         <button onClick={
+             () => Router.push("/auth/signup")}
+         >
+             Dont Have an account? Register here.
+         </button>
+
+        </div>
     )
    
 } 
--- frontend/src/pages/auth/signup/index.tsx
+++ frontend/src/pages/auth/signup/index.tsx
@@ -1,6 +1,8 @@
 import React, {useState} from "react"
 
-export const Register = (props: any) => {
+import Router from "next/router"
+
+export default function Register() {
     const[email, setEmail] = useState('');
     const[password,setPassword] = useState('');
     const[name, setName]= useState('');
@@ -16,12 +18,12 @@ export const Register = (props: any) => {
                 <label htmlFor="name">Full Name</label>
                 <input value ={name} name="name" id="name" placeholder="Full Name"/>
                 <label htmlFor="email">email</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)} type ="email" placeholder="email@whatevermail.com" id="email" name="email"/>
+                <input value={email} onChange={(e) => setEmail(e.target.value)} type="text" placeholder="email@whatevermail.com" id="email" name="email"/>
                 <label htmlFor="password">email</label>
                 <input value={password} onChange={(e) => setPassword(e.target.value)} type="passsword" placeholder="**********" id="password" name="name"/>
                 <button type="submit">Log In</button>
             </form>
-            <button onClick={() => props.onFormSwitch('login')}> Already have an account? Login here. </button>
+            <button onClick={() => Router.push("/auth/login")}> Already have an account? Login here. </button>
         </div>
     )
 }
