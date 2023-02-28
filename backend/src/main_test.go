bb93a57f5b85b0aabf6a5d1f9b20a6776e750181
NicholasOrth <orthn@ufl.edu>
2023-02-28 15:07:39 -0500

    Add: tests for GET for users, outline for "following" list

--- backend/src/main_test.go
+++ backend/src/main_test.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"golang.org/x/crypto/bcrypt"
 )
 
+// test the hash function
 func TestHashStr(t *testing.T) {
 	passwordTest := "TestPassword11@@"
 	hashed := HashStr(passwordTest)
@@ -15,6 +19,8 @@ func TestHashStr(t *testing.T) {
 	}
 }
 
+// test the add follower function
+// NOTE: not yet implemented as a built in function, instead just use append twice
 func TestAddFollower(t *testing.T) {
 	user := User{}
 	target := User{}
@@ -27,5 +33,45 @@ func TestAddFollower(t *testing.T) {
 	}
 	if len(target.Followers) != 1 {
 		t.Errorf("Target followers list not updated, length: %d", len(target.Followers))
+	}
+}
+
+func getUsersHandler(w http.ResponseWriter, r *http.Request) {
+	// retrieve list of users from database
+	users := []User{{Name: "Nick"}, {Name: "Larry"}}
+
+	// encode users as JSON and write to response
+	json.NewEncoder(w).Encode(users)
+}
+
+// test GET for users
+func TestGetUsers(t *testing.T) {
+	// create test server with getUsersHandler as handler for /users endpoint
+	server := httptest.NewServer(http.HandlerFunc(getUsersHandler))
+	defer server.Close()
+
+	// make GET request to /users endpoint
+	resp, err := http.Get(server.URL + "/users")
+	if err != nil {
+		t.Fatalf("Error making GET request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// decode response JSON into slice of User objects
+	var users []User
+	err = json.NewDecoder(resp.Body).Decode(&users)
+	if err != nil {
+		t.Fatalf("Error decoding response JSON: %v", err)
+	}
+
+	// verify that we received the expected list of users
+	if len(users) != 2 {
+		t.Fatalf("Expected 2 users, but got %d", len(users))
+	}
+	if users[0].Name != "Nick" {
+		t.Fatalf("Expected first user to be Nick, but got %s", users[0].Name)
+	}
+	if users[1].Name != "Larry" {
+		t.Fatalf("Expected second user to be Larry, but got %s", users[1].Name)
 	}
 }
