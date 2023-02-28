329f41fe145bf8eaedf487a0beb8a7b1ff58e47e
NicholasOrth <orthn@ufl.edu>
2023-02-28 15:22:03 -0500

    add unit testing for GET and POST

--- backend/src/main_test.go
+++ backend/src/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
@@ -36,6 +37,7 @@ func TestAddFollower(t *testing.T) {
 	}
 }
 
+// handeler for testing GET request
 func getUsersHandler(w http.ResponseWriter, r *http.Request) {
 	// retrieve list of users from database
 	users := []User{{Name: "Nick"}, {Name: "Larry"}}
@@ -44,6 +46,7 @@ func getUsersHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(users)
 }
 
+// API tests
 // test GET for users
 func TestGetUsers(t *testing.T) {
 	// create test server with getUsersHandler as handler for /users endpoint
@@ -73,5 +76,64 @@ func TestGetUsers(t *testing.T) {
 	}
 	if users[1].Name != "Larry" {
 		t.Fatalf("Expected second user to be Larry, but got %s", users[1].Name)
+	}
+}
+
+var users []User
+
+func createUserHandler(w http.ResponseWriter, r *http.Request) {
+	// decode JSON request body into User object
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	//add user to database
+	users = append(users, user)
+
+	// encode user as JSON and write to response
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(user)
+}
+
+func TestCreateUser(t *testing.T) {
+	// create test server with createUserHandler as handler for /users endpoint
+	server := httptest.NewServer(http.HandlerFunc(createUserHandler))
+	defer server.Close()
+
+	// create new user object
+	newUser := User{Name: "Charlie"}
+
+	// encode user as JSON
+	requestBody, err := json.Marshal(newUser)
+	if err != nil {
+		t.Fatalf("Error encoding request JSON: %v", err)
+	}
+
+	// make POST request to /users endpoint with JSON request body
+	resp, err := http.Post(server.URL+"/users", "application/json", bytes.NewBuffer(requestBody))
+	if err != nil {
+		t.Fatalf("Error making POST request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// verify that the response status code is 201 Created
+	if resp.StatusCode != http.StatusCreated {
+		t.Fatalf("Expected status code 201 Created, but got %v", resp.StatusCode)
+	}
+
+	// decode response JSON into User object
+	var createdUser User
+	err = json.NewDecoder(resp.Body).Decode(&createdUser)
+	if err != nil {
+		t.Fatalf("Error decoding response JSON: %v", err)
+	}
+
+	// verify that the created user matches the expected user
+	expectedUser := User{Name: "Nick"}
+	if createdUser.ID != expectedUser.ID {
+		t.Fatalf("Expected created user to be %+v, but got %+v", expectedUser.ID, createdUser.ID)
 	}
 }
