7e498a1eac62cfc968c00beb379d8c6a7e4a6cc0
TommyMac15 <Tmcmullen1@ufl.edu>
2023-03-01 10:24:25 -0500

    update config

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,20 +1,13 @@
 describe('Home page test', () => {
   it('checks home page accessible', () => {
-    cy.visit('http://localhost:3000/')
+    cy.visit('/')
   })
 })
 
 describe('Button click', () => {
   it('clicks the log in button', () => {
-    cy.visit('http://localhost:3000/')
+    cy.visit('/')
 
     cy.contains('Log In').click()
-  })
-})
-
-
-describe('sign up', () => {
-  it('checks sign up accessiblity', () => {
-    cy
   })
 })
