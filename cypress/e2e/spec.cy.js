e2f428f49cb8d447d6e24d52dbb5a2f10d2431ad
TommyMac15 <Tmcmullen1@ufl.edu>
2023-03-01 21:33:24 -0500

    additional tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,10 +4,46 @@ describe('Home page test', () => {
   })
 })
 
-describe('Button click', () => {
+describe('Login page test', () => {
+  it('checks login page accessible', () => {
+    cy.visit('/auth/login/')
+  })
+})
+
+describe('Signup page test', () => {
+  it('checks signup page accessible', () => {
+    cy.visit('/auth/signup/')
+  })
+})
+
+describe('Home page login button click', () => {
   it('clicks the log in button', () => {
     cy.visit('/')
 
     cy.contains('Log In').click()
+  })
+})
+
+describe('Home page signup button click', () => {
+  it('clicks the log in button', () => {
+    cy.visit('/')
+
+    cy.contains('Sign Up').click()
+  })
+})
+
+describe('Login page, signup button click', () => {
+  it('clicks the log in button', () => {
+    cy.visit('/auth/login/')
+
+    cy.contains('Dont Have an account? Register here.').click()
+  })
+})
+
+describe('Signup page, already have acct button click', () => {
+  it('clicks the log in button', () => {
+    cy.visit('/auth/signup/')
+
+    cy.contains('Already have an account? Login here.').click()
   })
 })
