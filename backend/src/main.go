6874fb669de34659ca8b14a13e71e1d82b1a7753
slinky55 <larry.mason@ufl.edu>
2023-04-12 02:47:42 -0400

    added a new origin for debug purposes

--- backend/src/main.go
+++ backend/src/main.go
@@ -162,7 +162,7 @@ func main() {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:3001"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
 		AllowHeaders:     []string{"Origin", "Content-Type"},
 		AllowCredentials: true,
