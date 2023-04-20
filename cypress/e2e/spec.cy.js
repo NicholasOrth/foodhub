9637f927cac3c193fcd00c7e9fc02d6c19a4fcfe
TommyMac15 <Tmcmullen1@ufl.edu>
2023-04-19 22:22:16 -0400

    testing additions

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -62,3 +62,94 @@ describe('Login and Logout', () => {
     cy.url().should('include', '/home')
   })
 })
+
+describe('Sign Up', () => {
+  beforeEach(() => {
+    cy.visit('/signup')
+  })
+
+  it('should require email', () => {
+    // Fill out the login form
+    cy.get('input[name="name"]').type('myName')
+    cy.get('input[email="email"]').type('myemail')
+    cy.get('input[password="password"]').type('matchingPassword1')
+    cy.get('input[confirm="password"]').type('matchingPassword1')
+    cy.get('button[type="submit"]').click()
+
+    cy.url().should('include', '/signup')
+  })
+})
+
+describe('Sign Up', () => {
+  beforeEach(() => {
+    cy.visit('/signup')
+  })
+
+  it('should match password', () => {
+    // Fill out the login form
+    cy.get('input[name="name"]').type('myName')
+    cy.get('input[email="email"]').type('myemail@email.com')
+    cy.get('input[password="password"]').type('matchingPassword1')
+    cy.get('input[confirm="password"]').type('notMatchingPassword1')
+    cy.get('button[type="submit"]').click()
+
+    cy.url().should('include', '/signup')
+  })
+})
+
+
+describe('Sign Up', () => {
+  beforeEach(() => {
+    cy.visit('/signup')
+  })
+
+  it('password contains 8 character', () => {
+    // Fill out the login form
+    cy.get('input[name="name"]').type('myName')
+    cy.get('input[email="email"]').type('myemail@email.com')
+    cy.get('input[password="password"]').type('shortP1')
+    cy.get('input[confirm="password"]').type('shortP1')
+    cy.get('button[type="submit"]').click()
+
+    cy.url().should('include', '/signup')
+  })
+})
+
+describe('Sign Up', () => {
+  beforeEach(() => {
+    cy.visit('/signup')
+  })
+
+  it('password contains upper', () => {
+    // Fill out the login form
+    cy.get('input[name="name"]').type('myName')
+    cy.get('input[email="email"]').type('myemail@email.com')
+    cy.get('input[password="password"]').type('lowercasepassword1')
+    cy.get('input[confirm="password"]').type('lowercasepassword1')
+    cy.get('button[type="submit"]').click()
+
+    cy.url().should('include', '/signup')
+  })
+})
+
+describe('Sign Up', () => {
+  beforeEach(() => {
+    cy.visit('/signup')
+  })
+
+  it('can sign up and log in', () => {
+    // Fill out the login form
+    cy.get('input[name="name"]').type('myName')
+    cy.get('input[email="email"]').type('myemail@email.com')
+    cy.get('input[password="password"]').type('correctPassword1')
+    cy.get('input[confirm="password"]').type('correctPassword1')
+    cy.get('button[type="submit"]').click()
+
+    cy.get('input[name="username"]').type('myemail@email.com')
+    cy.get('input[name="password"]').type('correctPassword1')
+    cy.get('button[type="submit"]').click()
+
+    // Check if the user is redirected to the home page after login
+    cy.url().should('include', '/home')
+  })
+})
